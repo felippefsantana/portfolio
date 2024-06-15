b9d762db8f607d2be76bf31ecc3034a53c16199f
Felippe Santana <felippefsantana02@gmail.com>
2024-06-14 21:53:03 -0300

    feat: about section

--- src/pages/Home/components/About.tsx
+++ src/pages/Home/components/About.tsx
@@ -1,18 +1,43 @@
 import { Separator } from "@/components/ui/separator"
+import { Quote } from "lucide-react"
 
 const About = () => {
   return (
     <section id="about">
-      <div className="container h-screen flex flex-col md:flex-row justify-evenly items-center py-8">
-        <div>
-          Escrever alguma coisa sobre mim!
+      <div className="container py-16">
+        <div className="mb-8">
+          <h2 className="text-4xl text-center md:text-6xl md:text-start">Quem sou eu?</h2>
         </div>
-        <Separator orientation="vertical" />
-        <div>
-          <p>Skills</p>
-          <ul>
-            <li>React JS</li>
-          </ul>
+
+        <div className="flex flex-col flex-wrap md:flex-row justify-between items-stretch space-y-4 gap-4">
+          <div className="basis-3/5">
+            <div className="text-lg md:text-2xl text-justify font-semibold">
+              <Quote className="rotate-180 inline align-baseline" width={54} height={48} />
+              <p className="inline indent-10">
+                Olá, me chamo Felippe Santana, tenho 21 anos e sou desenvolvedor fullstack. Iniciei minha jornada na área de tecnologia em 2018 ao ingressar numa escola profissionalizante no curso de técnico de informática, onde me interessei pelo desenvolvimento de software e por tudo que é possível fazer com ele.
+              </p>
+              <p className="indent-10 mt-5">
+                Atualmente sou desenvolvedor fullstack com mais de 3 anos de experiência profissional com desenvolvimento web. Possui conhecimentos em programação funcional, programação orientada a objetos (POO), linguagens de programação back end como Node JS e Laravel para aplicações escaláveis, além de sólidos conhecimentos em TypeScript, SQL e NoSQL databases, TDD e BDD, e proficiência em React JS/Next JS e Vue JS para desenvolvimento de interfaces de usuário.
+              </p>
+            </div>
+          </div>
+          <Separator orientation="vertical" className="hidden md:block h-96 self-center" />
+          <Separator className="md:hidden" />
+          <div className="basis-1/5">
+            <p className="font-medium text-center text-lg">Skills</p>
+            <ul className="font-semibold">
+              <li>React JS</li>
+              <li>Next JS</li>
+              <li>Vue JS</li>
+              <li>Node JS</li>
+              <li>Typescript JS</li>
+              <li>Tailwind CSS</li>
+              <li>PostgreSQL</li>
+              <li>MongoDB</li>
+              <li>Laravel</li>
+              <li>Docker</li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -1,3 +1,4 @@
+import { Separator } from '@/components/ui/separator';
 import { Button } from '@/components/ui/button';
 import { Github, Linkedin } from 'lucide-react';
 
@@ -8,9 +9,16 @@ const Hero = () => {
         <img className="rounded-full border-2 border-gray-800 w-80" src="https://avatars.githubusercontent.com/u/59585809?v=4" alt="felippe santana" />
         <div className="text-center">
           <h1 className="text-5xl mb-5">Felippe Santana</h1>
-          <p className="text-2xl font-semibold mb-8">Software Developer</p>
+          <p className="text-2xl font-semibold mb-2">Software Developer</p>
+          <div className="flex h-5 justify-center items-center space-x-4 text-sm">
+            <span>React JS</span>
+            <Separator orientation="vertical" />
+            <span>Node JS</span>
+            <Separator orientation="vertical" />
+            <span>TypeScript</span>
+          </div>
 
-          <div className="flex justify-center gap-2">
+          <div className="flex justify-center gap-2 mt-8">
             <Button asChild>
               <a href="https://www.linkedin.com/in/felippefsantana/" target="_blank">
                 <Linkedin size={16} className="me-2" />
